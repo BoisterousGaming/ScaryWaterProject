17e54c59f522ca5fb6da13ceceefae5aa81d8af3
Soumen Koley <soumenkoley1991@gmail.com>
2017-11-11 21:45:02 +0530

    Nov_11_2017_9:44PM_Commit

---
+++ Assets/ScaryWater/GameElements/GUI/Scripts/FarBGSrc.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FarBGSrc : MonoBehaviour 
+{
+    Vector3 mvInitialPosition;
+    Vector3 mvTempPos;
+
+    public Transform _PlayerTransform;
+
+    void Start()
+    {
+        mvInitialPosition = this.transform.position;    
+    }
+
+    void LateUpdate()
+    {
+        mvTempPos.x = mvInitialPosition.x;
+        mvTempPos.y = mvInitialPosition.y;
+        mvTempPos.z = _PlayerTransform.position.z + mvInitialPosition.z;
+
+        this.transform.position = mvTempPos;
+    }
+}
